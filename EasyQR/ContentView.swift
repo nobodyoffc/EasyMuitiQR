3a36155797852fe9dbd51e4555e5c5a46a9d5980
No1_NrC7 <nobodyoffc@gmail.com>
2025-03-06 09:52:20 +0800

    Initial Commit

---
+++ EasyQR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyQR
+//
+//  Created by 刘昌用 on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyQR/EasyQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyQRApp.swift
+//  EasyQR
+//
+//  Created by 刘昌用 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct EasyQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
